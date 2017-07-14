c6b06e539b49d6066737177c77a4e0977a04168e
Admin <Admin@Eliss-MacBook-Air.local>
2017-07-14 00:11:36 -0400

    completed Post routes for survey and additional user functionality

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,17 @@
+const path = require('path');
+
+const friends = require('../data/friends')
+
 module.exports = function(app) {
 
-
-    app.get('/api', function(req, res) {
-        res.send("I am werking now");
+    app.get('/api/survey', function(req, res) {
+        res.json(friends);
     });
 
+    app.post('/data/friends', function(req, res) {
+    	console.log("You're hittin the fucking ting")
+    	console.log(req.body)
+        // friendData.push(req.body);
+
+    });
 };
